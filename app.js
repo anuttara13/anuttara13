b4a9c1ec928fb40531a076dec86fb4fd79fc75c4
anuttara13 <49940671+anuttara13@users.noreply.github.com>
2019-08-08 17:32:04 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,39 @@
+// Reply with two static messages
+
+const express = require('express')
+const bodyParser = require('body-parser')
+const request = require('request')
+const app = express()
+const port = process.env.PORT || 4000
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.post('/webhook', (req, res) => {
+    let reply_token = req.body.events[0].replyToken
+    reply(reply_token)
+    res.sendStatus(200)
+})
+app.listen(port)
+function reply(reply_token) {
+    let headers = {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer {e8ZXcPl6clb/udyBilk4DSUQkJ44XTz3S3MdZdn9HTCNeK+r3WG6qmR3lybM67Hc0oMDHStxES+h/yS9f2lhsRSwxWrwGPiUJFaejSAsta6USvFCy51lctlLWg2QKHgT/9s3qKz+0WtmcdEIOI7YdQdB04t89/1O/w1cDnyilFU=}'
+    }
+    let body = JSON.stringify({
+        replyToken: reply_token,
+        messages: [{
+            type: 'text',
+            text: 'Hello'
+        },
+        {
+            type: 'text',
+            text: 'How are you?'
+        }]
+    })
+    request.post({
+        url: 'https://api.line.me/v2/bot/message/reply',
+        headers: headers,
+        body: body
+    }, (err, res, body) => {
+        console.log('status = ' + res.statusCode);
+    });
+}
